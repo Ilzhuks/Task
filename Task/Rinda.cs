f45b0be35db72e7e012944fd88990f8384c8c7af
Ilzhuks <ilze.vilcina7@gmail.com>
2022-08-04 13:04:49 +0300

    task

--- Task/Rinda.cs
+++ Task/Rinda.cs
@@ -12,21 +12,11 @@ namespace HomeWork
         {
         }
 
-        //public Rinda(int v)
-        //{
-        //}
-
-        //public Rinda(string v)
-        //{
-        //}
 
         public string lineCode { get; set; }
         public int sum { get; set; }
 
-        //int[] lineSum =  new int[9] {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        // Random random = new Random();
-        //  int randomNumber = lineSum[Random.Next(lineSum.Length)];
-
+  
 
 
 
